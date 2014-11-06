f3e90dd67100d1ffe6a8ff9ede88345f7baa759c
mattinmir <mattinmir@users.noreply.github.com>
2014-11-06 12:04:45 +0000

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,56 +1,43 @@
-/*
- * source.cpp
- *
- *  Created on: 30 Oct 2014
- *      Author: mm5213
- */
-
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <cmath>
 #include <vector>
-#include "helpers.h"
+#include "classHeader.hpp"
 
 using namespace std;
 
 int get_farthest_point(vector<point> points);
+int member_point_unordsearch(point p, vector<point> v);
+int member_point_binsearch(point p, vector<point> v);
+
 
 int main()
 {
-
 	vector<point> points;
 
 	point p1(2, 2);
 	point p2(3, 3);
 	point p3(1, 1);
+	point p4(8, 7);
+	point p5(3, 4);
+	point p6(-6, 1);
 
 	points.push_back(p1);
 	points.push_back(p2);
 	points.push_back(p3);
+	points.push_back(p4);
+	points.push_back(p5);
+	points.push_back(p6);
 
-	cout << "Farthest point index: " << get_farthest_point(points) << endl;
-
-	char x, y;
-	if(p1<p2)
-		x = 'T';
-	else
-		x = 'F';
-	if (p1==p2)
-		y = 'T';
-	else
-		y = 'F';
-	cout << "P1 < P2 is: " << x << endl;
-	cout << "P1 == P2 is: " << y << endl;
-
-
+	cout << member_point_unordsearch (point(4,3), points)<< endl;
 	return 0;
 }
 
 int get_farthest_point(vector<point> points)
 {
 	int index = 0;
-	for (int i = 1; i < points.size(); i++)
+	for (unsigned int i = 1; i < points.size(); i++)
 	{
 		if (points[i].get_magnitude() > points[index].get_magnitude())
 			index = i;
@@ -58,3 +45,43 @@ int get_farthest_point(vector<point> points)
 
 	return index;
 }
+
+int member_point_unordsearch(point p, vector<point> v)
+{
+	unsigned int index = 0;
+
+	for (index = 0; index < v.size(); index++)
+	{
+		if (v.at(index) == p)
+				return index;
+	}
+
+	return v.size();
+}
+/*
+int member_point_binsearch(vector<point> v, point p, int imin, int imax)
+{
+
+	// test if array is empty
+	  if (imax < imin)
+	    // set is empty, so return value showing not found
+	    return v.size();
+	  else
+	    {
+	      // calculate midpoint to cut set in half
+	      int imid = (imax - imin) / 2;
+
+	      // three-way comparison
+	      if (v[imid] > key)
+	        // key is in lower subset
+	        return member_point_binsearch(v, key, imin, imid-1);
+	      else if (v[imid] < key)
+	        // key is in upper subset
+	        return binary_search(A, key, imid+1, imax);
+	      else
+	        // key has been found
+	        return imid;
+	    }
+
+
+}*/
